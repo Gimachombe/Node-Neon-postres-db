2c26e91797dfa8289af2cd24a57ece2c7aa7c508
Gimachombe <jacobgimachombe@gmail.com>
2025-10-16 23:31:21 +0300

    added a post route to add to database

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const { Pool } = require('pg');
-
 const app = express();
-app.use(cors());
-app.use(express.json());
+
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGSSLMODE, PGCHANNELBINDING } = process.env;
 
@@ -21,19 +19,44 @@ const pool = new Pool({
   },
 });
 
-pool.connect().then(() => console.log("Connected to PostgreSQL"))
-.catch(err => console.log("Databse connection error:", err.message));
+app.use(cors());
+app.use(express.json());
 
+pool.connect().then( client => {
+  console.log("Connected to PostgreSQL");
+  client.release();
+})
+.catch(err => console.log("Databse connection error:", err.stack));
 
+//Get route
 app.get('/', async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM posts');
-  res.json(rows);
+    const { result } = await pool.query('SELECT * FROM posts ORDER BY id DESC');
+  res.json(result.rows);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Database query failed"});
   }
 });
 
+//Post route
+app.post("/posts", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content)
+  return res.status(400).json({ error: 'Title and content are required' });
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO posts (title, content) VALUES ($1, $2) RETURNING *',
+      [title, content]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error('Database insert error:', error);
+    res.status(500).json({ error: 'Failed to insert post' });
+  }
+});
 
 app.listen(3000, () => console.log('Server Running on http://localhost:3000'));
